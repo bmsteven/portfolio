ebc46d6b8e5c3344ad4c9e7b386fe9cf22e51331
bmsteven <bmsteve96@gmail.com>
2019-08-02 02:21:04 +0300

    Updates

---
+++ precache-manifest.6428e99a966fb7f683244dd56621d9e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bc460e29e2f1607c66e19540cc1a6d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fe259a602df40ebcf884",
+    "url": "/portfolio/static/css/main.91fe03c1.chunk.css"
+  },
+  {
+    "revision": "90f0d0ba85256f5624d3",
+    "url": "/portfolio/static/js/2.d190fba0.chunk.js"
+  },
+  {
+    "revision": "fe259a602df40ebcf884",
+    "url": "/portfolio/static/js/main.8fc73d59.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e447954c6913201b57ceb06de607b983",
+    "url": "/portfolio/static/media/bg.e447954c.jpg"
+  },
+  {
+    "revision": "a50b04a91c794074ce359f546746184c",
+    "url": "/portfolio/static/media/bgpt.a50b04a9.jpg"
+  },
+  {
+    "revision": "d2860a1d4180d5bc8ffac4e54aa9333b",
+    "url": "/portfolio/static/media/blog.d2860a1d.png"
+  },
+  {
+    "revision": "ee8791b80b5c3fbacb79478e1cee1fc2",
+    "url": "/portfolio/static/media/contact bg.ee8791b8.jpg"
+  },
+  {
+    "revision": "5e701c583c73847930fa0099ae84773f",
+    "url": "/portfolio/static/media/dp.5e701c58.png"
+  },
+  {
+    "revision": "528f7e9f567ac31a47fb4a53021ee2cf",
+    "url": "/portfolio/static/media/kilimoshare.528f7e9f.png"
+  },
+  {
+    "revision": "28e4de1a99db928d712d7e84c627b8ae",
+    "url": "/portfolio/static/media/mobBg.28e4de1a.jpg"
+  },
+  {
+    "revision": "39177f2f71fde61adbc9fc0414b9ac3c",
+    "url": "/portfolio/static/media/qsapp.39177f2f.png"
+  }
+]);
